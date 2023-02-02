ae5b47c10b3f388f3b3f410cd14bbf8969a47e09
John Powers <jpowers@flexgen.com>
2023-02-02 13:03:46 -0500

    add arrows

---
+++ src/components/ExperienceBox.js
@@ -0,0 +1,11 @@
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+
+export default function ExperienceBox(props) {
+    return (
+        <>
+        <ArrowLeftIcon fontSize="large" />
+        <ArrowRightIcon fontSize="large" />
+        </>
+    )
+}
--- src/views/Casual.js
+++ src/views/Casual.js
@@ -1,13 +1,14 @@
 import {useState} from 'react';
 import { Container } from "@mui/material";
 import Ticker from "../components/Ticker";
-
+import ExperienceBox from '../components/ExperienceBox';
 export default function Casual() {
   return (
     <>
     <Container>
         <h1>casual</h1>
         <Ticker arr={["one","two","three","four","five","six","seven"]}/>
+        <ExperienceBox/>
     </Container>
     </>
   )
