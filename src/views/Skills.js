beb9e44e53f0311e8b4b8cc71d703e51a3451e43
John Powers <jpowers@flexgen.com>
2023-02-06 13:29:04 -0500

    mobile

--- src/views/Skills.js
+++ src/views/Skills.js
@@ -1,4 +1,5 @@
 import {Box, Grid} from "@mui/material";
+import { isMobile } from "react-device-detect";
 import SkillsBox from "../components/SkillsBox";
 import SkillsList from "../components/SkillsList";
 
@@ -6,8 +7,15 @@ export default function Skills() {
     return (
         <Grid container
         >
-            <Grid item sx={12} md={4}><SkillsList/></Grid>
-            <Grid item sx={0} md={8}><SkillsBox/></Grid>
+            
+            {(isMobile === false) ? 
+                <>
+                    <Grid item sx={12} md={4}><SkillsList/></Grid>
+                    <Grid item sx={0} md={8}><SkillsBox/></Grid>
+                </>
+                :
+                <Grid item sx={12} md={4}><SkillsList/></Grid>
+            }
             
         </Grid>
     )
