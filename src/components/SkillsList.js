795d26e07ddaaa72ed419da1cff059c9bb982f91
John Powers <john.powers070@gmail.com>
2023-02-15 09:45:38 -0500

    start modal work for skills

--- src/components/SkillsList.js
+++ src/components/SkillsList.js
@@ -1,37 +1,111 @@
-import {useEffect, useState} from 'react'; 
-import {Grid, Typography} from "@mui/material";
+import { useEffect, useState } from "react";
+import { Grid, Typography, Button } from "@mui/material";
+import SkillsModal from "./SkillsModal";
+
+const emails = ["username@gmail.com", "user02@gmail.com"];
 
 export default function SkillsList(props) {
-    let [variant, setVariant] = useState("h6");
-    const skills = [
-                    ["React.js","Regular Expressions"],
-                    ["bug fix","Material UI"],
-                    ["Docker","Docker Compose"],
-                    ["Security","Vue.js"],
-                    ["Node.js","Typescript"],
-                    ["Agile","Python"],
-                    ["R & D", "PWA"],
-                    ["MongoDB", "Postgres"]
+  let [variant, setVariant] = useState("h6");
+  let [open, setOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState([]);
+  const skills = [
+    [
+      { name: "React.js", text: "hello react" },
+      { name: "Regular Expressions", text: "hello regex" }
+    ],
+    [
+      { name: "bug fix", text: "" },
+      { name: "Material UI", text: "" }
+    ],
+    [
+      { name: "Docker", text: "" },
+      { name: "Docker Compose", text: "" }
+    ],
+    [
+      { name: "Security", text: "" },
+      { name: "Vue.js", text: "" }
+    ],
+    [
+      { name: "Node.js", text: "" },
+      { name: "Typescript", text: "" }
+    ],
+    [
+      { name: "Agile", text: "" },
+      { name: "Python", text: "" }
+    ],
+    [
+      { name: "R & D", text: "" },
+      { name: "PWA", text: "" }
+    ],
+    [
+      { name: "MongoDB", text: "" },
+      { name: "Postgres", text: "" }
     ]
-    useEffect(()=>{
-        if(props.mobile === true) {
-            setVariant("p")
-        }
-    },[])
-    return (
-        <>
-        <Grid container>
-            {skills.map(skill=>{
-                return (
-                    <>
-                        <Grid item style={{marginBottom:"1em", border: "2px solid #d6d6d6", padding: "1.2em"}} sx={6} md={5}><Typography variant={variant}><em>{skill[0]}</em></Typography></Grid> 
-                        <Grid item sx={2} md={2}></Grid>   
-                        <Grid item style={{marginBottom:"1em", border: "2px solid #d6d6d6", padding: "1.2em"}} sx={6} md={5}><Typography variant={variant}><em>{skill[1]}</em></Typography></Grid>
-                        <br/>
-                    </>
-                )
-            })}
-        </Grid>
-        </>
-    )
+  ];
+  const handleButtonClick = value => {
+    setSelectedValue(value);
+    setOpen(true);
+  };
+  const handleClose = value => {
+    setOpen(false);
+    setSelectedValue(value);
+  };
+  useEffect(() => {
+    if (props.mobile === true) {
+      setVariant("p");
+    }
+  }, []);
+  return (
+    <>
+      <SkillsModal
+        selectedValue={selectedValue}
+        open={open}
+        onClose={handleClose}
+      />
+      <Grid container>
+        {skills.map((skill, index) => {
+          return (
+            <>
+              <Grid
+                item
+                style={{
+                  marginBottom: "1em",
+                  border: "2px solid #d6d6d6",
+                  padding: "1.2em"
+                }}
+                sx={6}
+                md={5}
+              >
+                <Button onClick={() => handleButtonClick(skills[index][0])}>
+                  <Typography variant={variant}>
+                    <em>{skill[0].name}</em>
+                  </Typography>
+                </Button>
+              </Grid>
+
+              <Grid item sx={2} md={2}></Grid>
+              <Grid
+                item
+                style={{
+                  marginBottom: "1em",
+                  border: "2px solid #d6d6d6",
+                  padding: "1.2em"
+                }}
+                sx={6}
+                md={5}
+              >
+                <Button onClick={() => handleButtonClick(skills[index][1])}>
+                  <Typography variant={variant}>
+                    <em>{skill[1].name}</em>
+                  </Typography>
+                </Button>
+              </Grid>
+
+              <br />
+            </>
+          );
+        })}
+      </Grid>
+    </>
+  );
 }
---
+++ src/components/SkillsModal.js
@@ -0,0 +1,40 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+import PersonIcon from "@mui/icons-material/Person";
+import AddIcon from "@mui/icons-material/Add";
+import Typography from "@mui/material/Typography";
+import { blue } from "@mui/material/colors";
+
+const emails = ["username@gmail.com", "user02@gmail.com"];
+
+export default function SkillsModal(props) {
+  const { onClose, selectedValue, open } = props;
+
+  const handleClose = () => {
+    onClose(selectedValue);
+  };
+
+  const handleListItemClick = value => {
+    onClose(value);
+  };
+
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle>{selectedValue.name}</DialogTitle>
+      <List sx={{ pt: 0, pl: "2em", pr: "2em" }}>
+        <ListItem disableGutters>
+          <ListItemText primary={selectedValue.text} />
+        </ListItem>
+      </List>
+    </Dialog>
+  );
+}
