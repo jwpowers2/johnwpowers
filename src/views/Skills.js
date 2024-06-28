66b6d782308a1df4ae0c9723a117dc3e344d935c
John Powers <john.powers070@gmail.com>
2024-06-28 10:56:36 -0400

    mobile skills setup

--- src/views/Skills.js
+++ src/views/Skills.js
@@ -1,9 +1,17 @@
 import {Box, Grid} from "@mui/material";
-import { isMobile } from "react-device-detect";
+import {useState, useEffect} from 'react';
+//import { isMobile } from "react-device-detect";
 import SkillsBox from "../components/SkillsBox";
 import SkillsList from "../components/SkillsList";
+import MobileSkillsList from "../components/MobileSkillsList";
 
 export default function Skills() {
+    let [isMobile, setIsMobile] = useState(false);
+
+    useEffect(()=>{
+      if (window.innerWidth< 1024) console.log("WINDOW IS LESS THAN 700")
+      setIsMobile(window.innerWidth < 1200);
+    },[])
     return (
         <Grid container className="card-background"
         >
@@ -16,7 +24,7 @@ export default function Skills() {
                 :
                 <>
                     
-                    <Grid item sx={12} md={12}><SkillsList mobile={true} /></Grid>
+                    <Grid item sx={12} md={12}><MobileSkillsList mobile={true} /></Grid>
                 </>
             }
             
